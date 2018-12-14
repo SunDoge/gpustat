47eb8b7abceaa8e560cfb2a93961a11179583b68
SunDoge <384813529@qq.com>
2018-12-14 19:06:04 +0800

    finish first stage, TODO: impl Display

--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,74 @@ extern crate serde_derive;
 
 use chrono::{DateTime, Local};
 use hostname::get_hostname;
+use nvml_wrapper::enums::device::UsedGpuMemory;
 use nvml_wrapper::error::Result;
 use nvml_wrapper::struct_wrappers::device::{MemoryInfo, ProcessInfo, Utilization};
 use nvml_wrapper::Device;
 use nvml_wrapper::NVML;
 
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::Path;
+
+pub fn get_docker_container_id(pid: u32) -> Option<String> {
+    let cgroup = format!("/proc/{}/cgroup", pid);
+    if Path::new(&cgroup).exists() {
+        let f = File::open(&cgroup).unwrap();
+        let file = BufReader::new(&f);
+        for line in file.lines() {
+            let line = line.unwrap();
+            if line.contains("docker") {
+                let line = line.replace("\n", "");
+                let docker_path = line.split(":").last().unwrap();
+                let id = docker_path.split("/").last().unwrap();
+                return Some(id.to_string());
+            }
+        }
+    }
+    None
+}
+
+pub fn get_processes(device: &Device) -> Vec<Process> {
+    let mut processes = Vec::new();
+
+    if let Ok(running_compute_processes) = device.running_compute_processes() {
+        for process_info in running_compute_processes {
+            processes.push(Process::from(process_info));
+        }
+    }
+
+    processes
+}
+
 #[derive(Debug, Serialize)]
 pub struct Process {
     pid: u32,
-    gpu_memory_usage: u32,
+    gpu_memory_usage: Option<u64>,
     command: String,
     username: String,
-    container: String,
+    container_id: Option<String>,
+}
+
+impl From<ProcessInfo> for Process {
+    fn from(process_info: ProcessInfo) -> Self {
+        let process = psutil::process::Process::new(process_info.pid as i32).unwrap();
+
+        Process {
+            pid: process_info.pid,
+            gpu_memory_usage: match process_info.used_gpu_memory {
+                UsedGpuMemory::Used(usage) => Some(usage),
+                UsedGpuMemory::Unavailable => None,
+            },
+            command: process.comm,
+            username: users::get_user_by_uid(process.uid)
+                .unwrap()
+                .name()
+                .to_string_lossy()
+                .into_owned(),
+            container_id: get_docker_container_id(process_info.pid),
+        }
+    }
 }
 
 #[derive(Debug, Serialize)]
@@ -24,23 +80,18 @@ pub struct GPUStat {
     name: String,
     memory: Option<MemoryInfo>,
     utilization: Option<Utilization>,
-    processes: Vec<ProcessInfo>,
+    processes: Vec<Process>,
 }
 
 impl GPUStat {
     pub fn new(index: u32, device: &Device) -> GPUStat {
-        let processes = device.running_compute_processes().unwrap_or(Vec::new());
-        for process in processes {
-            println!("{:?}", psutil::process::Process::new(process.pid as i32));
-        }
-
         GPUStat {
             index: index,
             uuid: device.uuid().unwrap(),
             name: device.name().unwrap(),
             memory: device.memory_info().ok(),
             utilization: device.utilization_rates().ok(),
-            processes: device.running_compute_processes().unwrap_or(Vec::new()),
+            processes: get_processes(device),
         }
     }
 
@@ -53,9 +104,7 @@ impl GPUStat {
             self.utilization = device.utilization_rates().ok();
         }
 
-        if let Ok(processes) = device.running_compute_processes() {
-            self.processes = processes;
-        }
+        self.processes = get_processes(device);
     }
 }
 
